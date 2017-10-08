8b952637390a3e8c86565565839a8a9b2344c951
Surapee <yamamoto_SUSHI@HOTMAIL.COM>
2017-10-09 01:14:24 +0700

    Update command.py

--- src/cocoa/toga_cocoa/command.py
+++ src/cocoa/toga_cocoa/command.py
@@ -6,8 +6,7 @@ from toga.widgets.icon import Icon
 class Command:
     def __init__(self, interface):
         self.interface = interface
+        self.icon = None
 
         if self.interface.icon_id:
             self.icon = Icon.load(self.interface.icon_id)
-        else:
-            self.icon = None
